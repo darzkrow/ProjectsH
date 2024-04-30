221e5731e2f34de569ce39625afd29f0b7e3e75b
DarzKrow <gfrancojf@gmail.com>
2024-04-29 21:21:32 -0400

    corregido los media file

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static/'),
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
