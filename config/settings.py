b849c605158163fd1508682373c22487fb25a153
DarzKrow <gfrancojf@gmail.com>
2024-05-01 16:18:34 -0400

    agregando css y plugins faltantes

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(4m3-f0z3l^onf)&5_*^fv)=-5qwi$ewp=bsi79wz35^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
